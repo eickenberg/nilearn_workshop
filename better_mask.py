082ccd5af61336a9e0e8639427d98ab76fdcd3be
banilo <danilobzdok@gmail.com>
2016-01-14 13:25:54 +0100

    add some plotting

--- better_mask.py
+++ better_mask.py
@@ -5,3 +5,5 @@ mask = masker.mask_img_.get_data()
 print(mask.shape)
 plt.matshow(mask[20])
 
+plot_roi(masker.mask_img_)
+
--- show_naive_mask.py
+++ show_naive_mask.py
@@ -2,3 +2,6 @@ mask = masker.mask_img_.get_data()
 print(mask.shape)
 plt.matshow(mask[20])
 
+from nilearn.plotting import plot_roi
+plot_roi(masker.mask_img_, bg_img=None)
+
